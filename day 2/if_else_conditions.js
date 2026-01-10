88188376e72704619e507d1dd147fe222ad0951e
Yash_tambade <yashtambade56@gmail.com>
2026-01-10 16:11:13 +0530

    Create if_else_conditions.js

---
+++ day 2/if_else_conditions.js
@@ -0,0 +1,18 @@
+// if (true //
+// if (conditions) // do something
+
+let age = 19;
+if (age >= 18) console.log("adult");
+
+if (age > 19) {
+    console.log("adult!");
+    console.log("adult!");
+}
+
+if (age > 18) {
+    console.log("adult!");
+}else if (age === 18) {
+    console.log("come next year");
+}else {
+    console.log("under age");
+}
