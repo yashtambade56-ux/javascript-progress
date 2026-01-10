40656cca76ba3a98436f2198b828a09b102681ca
Yash_tambade <yashtambade56@gmail.com>
2026-01-10 16:12:00 +0530

    Create functions.js

---
+++ day 2/functions.js
@@ -0,0 +1,29 @@
+function function_name(para) {
+    // do something
+    console.log("");
+}
+
+function sumofTwoNumbers(para1, para2) {
+    return para1 + para2;
+}
+
+const sumoftwonumberusingarrowfunction = (para1, para2) => para1 + para2;
+
+console.log(sumofTwoNumbers(1, 2));
+console.log(sumoftwonumberusingarrowfunction(1, 2));
+sumofTwoNumbers;
+
+function sayHello(fname, lname) {
+    console.log(`Hello ${fname} ${lname}`);
+}
+
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+sayHello("yash", "tambade");
+
+let greet = function(fname, lname) {
+    console.log(`Hello ${fname} ${lname}`);
+};
+greet("yash");
